d9cf653329f8e872fa8e067b6ec8c5066c572404
Ankur Nigam <AN20695569@wipro.com>
2026-06-23 09:11:35 +0530

    updated demo.java file

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ package gitDemo;
 public class demo {
     public static void main(String[] args) {
         System.out.println("Hello java , this is github simple initialization");
+        System.out.println("New line added here");
     }
 }
